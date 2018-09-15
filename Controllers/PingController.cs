2a4275aaa4997f0fe2a01a56c4e8017bed5309d7
Adrian Ochmann <adrian.ochmann@outlook.com>
2018-09-15 12:58:49 +0200

    Added initial project

---
+++ Controllers/PingController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotnetBackend.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PingController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<string> Ping() => "pong";
+    }
+}
